40354f3cf5b615ccea97f7327208d266d7c685cb
Zhi Li (SC/DAA PhD) September 2017 <zl382@hilbert.esc.cam.ac.uk>
2018-09-17 15:28:53 +0100

    download bash release

--- data_processing/download.sh
+++ data_processing/download.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
-# Edit the event parameter in download_data_one_event.py
-event="20161225"
-# python3 download_data_one_event.py $event
-echo "Event $event Downloaded!"
-#python3 data_processing.py $event > output_download.txt
-echo "Event $event Processed!"
-python3 add_event_cmt.py $event #>> output_download.txt
-echo "Event $event CMT Added!!"
-python3 add_travel_times.py $event S P Sdiff Pdiff SKS SKKS #>> output_download.txt
-echo "Travel Time Added!!"
-python3 add_pierce_points.py $event 2600. Sdiff #>> output_download.txt
-echo "Pierce Points Added!!"
-python3 add_synthetics_topickle_extra.py $event
+# Edit the event parameter in download_data_one_event.py, 
+# then go to CMT search find the CMTSOLUTION FORMAT, 
+# copy that to the event directory, and run this script after that.
+event="20161225_test"
+STARTTIME_GLOBAL=$(date +%s)
+echo "This is OUTPUT file for downloading!" > OUTPUT
 
 
+STARTTIME=$(date +%s)
+python3 -u download_data_one_event.py $event >> OUTPUT
+tail -1 output_download.txt &
+echo "Event $event Downloaded in $[$ENDTIME - $STARTTIME] s!"
+
+STARTTIME=$(date +%s)
+python3 -u data_processing.py $event >> OUTPUT
+ENDTIME=$(date +%s)
+echo "Event $event Processed in $[$ENDTIME - $STARTTIME] s!"
+
+STARTTIME=$(date +%s)
+python3 -u add_event_cmt.py $event >> OUTPUT
+ENDTIME=$(date +%s)
+echo "Event $event CMT Added in $[$ENDTIME - $STARTTIME] s!"
+
+STARTTIME=$(date +%s)
+python3 -u add_travel_times.py $event S P Sdiff Pdiff SKS SKKS >> OUTPUT
+ENDTIME=$(date +%s)
+echo "Travel Time Added in $[$ENDTIME - $STARTTIME] s!"
+
+STARTTIME=$(date +%s)
+python3 -u add_pierce_points.py $event 2600. Sdiff >> OUTPUT
+ENDTIME=$(date +%s)
+echo "Pierce Points Added in $[$ENDTIME - $STARTTIME] s!"
+
+STARTTIME=$(date +%s)
+python3 -u add_synthetics_topickle_extra.py $event >> OUTPUT
+ENDTIME=$(date +%s)
+echo "Synthetics Added in $[$ENDTIME - $STARTTIME] s!"
 
 
+echo "Event $event Downloaded and Procesed Totally in $[$ENDTIME - $STARTTIME_GLOBAL] s!"
